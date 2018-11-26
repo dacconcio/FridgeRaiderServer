a4cd1cd5ae242ddd300b5b02b7b2a2dca69d45e7
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-11-26 12:36:56 -0500

    Category Tests

--- test/category.spec.js
+++ test/category.spec.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
-const { sync, createNode, Models } = require('../db')
+const { sync, createNode, findNode, Models } = require('../db')
 const app = supertest(require('../app'));
 
 describe('Category Routes - /api/categories/', () => {
@@ -12,7 +12,7 @@ describe('Category Routes - /api/categories/', () => {
           createNode(Models.Category, { name: "Breakfast" }),
           createNode(Models.Category, { name: "Lunch" }),
           createNode(Models.Category, { name: "Dinner" }),
-       ]);
+       ])
     });
   }); 
 
@@ -25,4 +25,33 @@ describe('Category Routes - /api/categories/', () => {
           expect(response.text).to.contain('Dinner');
       });
   });
+
+  it('Gets the category by Id', () => {
+    return findNode(Models.Category, { name: "Breakfast" })
+      .then(category => {
+        return app.get(`/api/categories/${category.id}`)
+          .expect(200)
+          .then(response => {
+            expect(response.text).to.contain('Breakfast');
+            expect(response.text).to.not.contain('Lunch');
+            expect(response.text).to.not.contain('Dinner');
+          });
+      })
+  });
+
+  it('Updates the category for given Id', () => {
+    return findNode(Models.Category, { name: "Breakfast" })
+      .then(category => {
+        return app.put(`/api/categories/${category.id}`)
+          .send({ name: "Dessert" })
+          .expect(303)
+      })
+  });
+
+  it('Creates category', () => {
+    return app.post('/api/categories/')
+      .send({ name: "Dessert" })
+      .expect(302)
+  });
+
 });
