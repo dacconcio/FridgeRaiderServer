c46bbc00de887c0efceaa156c0a17958637ee512
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-11-26 13:10:25 -0500

    Ingredient Tests

--- api/ingredients.js
+++ api/ingredients.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const { findAllNodes, Models } = require('../db')
+const { findAllNodes, findNode, updateNode, Models } = require('../db')
 const router = express.Router();
 
 router.get('/', (req, res, next) => { 
 
   findAllNodes(Models.Ingredient)
     .then(ingredients => res.send(ingredients))
+    .catch(next);
+
+});
+
+router.get('/:id', (req, res, next) => { 
+
+  findNode(Models.Ingredient, { id: req.params.id })
+    .then(ingredient => res.send(ingredient))
+    .catch(next);
+
+});
+
+router.put('/:id', (req, res, next) => { 
+  const { tags } = req.body;
+  updateNode(Models.Ingredient, {id: req.params.id}, { tags })
+    .then(() => res.redirect(303, `/api/ingredients/${req.params.id}`))
     .catch(next);
 
 });
