da117cbcb02a1304d30ca90b3496e743e3a804ae
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-11-26 22:59:01 -0500

    Reading key from config

--- api/users.js
+++ api/users.js
@@ -1,59 +1,35 @@
 const express = require('express');
-const {
-  findAllNodes,
-  findNode,
-  findRelationships,
-  createRelationship,
-  deleteRelationship,
-  Models,
-  Relationships,
-  createNode,
-  updateNode
-} = require('../db');
+const { findAllNodes, findNode, findRelationships, createRelationship, deleteRelationship,
+  Models, Relationships, createNode, updateNode } = require('../db')
+const admin = require('firebase-admin');
 const router = express.Router();
 
-const { firebaseApiKey } = require('../env.js');
+const { FIREBASE_API_KEY } = require('../config'); 
 
-let admin = require('firebase-admin');
-
-let config = {
-  apiKey: firebaseApiKey,
+admin.initializeApp({
+  apiKey: FIREBASE_API_KEY,
   authDomain: 'fridgeraider-d65aa.firebaseapp.com',
   projectId: 'fridgeraider-d65aa'
-};
-
-admin.initializeApp(config);
+});
 
 router.get('/:id', (req, res, next) => {
   let result;
+  findNode(Models.User, {id: req.params.id })
   findNode(Models.User, { id: req.params.id })
     .then(user => {
       result = user;
       return Promise.all([
-        findRelationships(
-          Models.User,
-          `n.id='${result.id}'`,
-          Relationships.HAS_SAVED,
-          'direction_out',
-          Models.Recipe
-        ),
-        findRelationships(
-          Models.User,
-          `n.id='${result.id}'`,
-          Relationships.HAS_POSTED,
-          'direction_out',
-          Models.Recipe
-        )
-      ]);
-    })
-    .then(([savedRecipes, postedRecipes]) => {
+        findRelationships(Models.User, `n.id='${result.id}'`, Relationships.HAS_SAVED, 'direction_out', Models.Recipe),
+        findRelationships(Models.User, `n.id='${result.id}'`, Relationships.HAS_POSTED, 'direction_out', Models.Recipe),
+      ])
+    }).then(([savedRecipes, postedRecipes]) => {
       result = { ...result, savedRecipes, postedRecipes };
       res.send(result);
-    })
-    .catch(next);
+    }).catch(next);
 });
 
 router.post('/firebase-auth/', async (req, res, next) => {
+
   try {
     const decodedToken = await admin.auth().verifyIdToken(req.body.idToken);
 
@@ -63,13 +39,12 @@ router.post('/firebase-auth/', async (req, res, next) => {
 
     let result;
     const user = await findNode(Models.User, { email: decodedToken.email });
-    result = user;
     if (!user) {
       return res.send({});
     }
 
-    const savedRecipes = await findRelationships(
-      Models.User,
+    result = user;
+    const savedRecipes = await findRelationships(Models.User,
       `n.id='${result.id}'`,
       Relationships.HAS_SAVED,
       'direction_out',
@@ -106,25 +81,17 @@ router.post('/', (req, res, next) => {
 });
 
 router.put('/:id/saveRecipe/:recipeId', (req, res, next) => {
-  createRelationship(
-    { model: Models.User, params: { id: req.params.id } },
-    { model: Models.Recipe, params: { id: req.params.recipeId } },
-    Relationships.hasSaved
-  )
+  createRelationship({ model: Models.User, params: { id: req.params.id } },
+      { model: Models.Recipe, params: { id: req.params.recipeId } }, Relationships.hasSaved)
     .then(() => res.redirect(303, `/api/users/${req.params.id}`))
     .catch(next);
 });
 
 router.put('/:id/unSaveRecipe/:recipeId', (req, res, next) => {
-  deleteRelationship(
-    Models.User,
-    Models.Recipe,
-    `n.id='${req.params.id}' and t.id='${req.params.recipeId}'`,
-    Relationships.HAS_SAVED,
-    'direction_out'
-  )
-    .then(() => res.redirect(303, `/api/users/${req.params.id}`))
-    .catch(next);
+  deleteRelationship(Models.User, Models.Recipe, `n.id='${req.params.id}' and t.id='${req.params.recipeId}'`,
+    Relationships.HAS_SAVED, 'direction_out'
+  ).then(() => res.redirect(303, `/api/users/${req.params.id}`))
+   .catch(next);
 });
 
 router.put('/:id', (req, res, next) => {
