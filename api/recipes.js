0b33260c31e9f479c159009a419f7b68bd1cdcc9
Jarret Rose <jarretrose@gmail.com>
2018-11-28 11:04:44 -0500

    restored neode code and implemented descending ordering with  distinct results

--- api/recipes.js
+++ api/recipes.js
@@ -23,50 +23,28 @@ router.get('/:id', (req, res, next) => {
     }).catch(next)
 });
 
-// router.get('/', async (req, res, next) => { 
-//   let recipes;
-//   const { ingredients } = req.query;
-//   try {
-//     if(ingredients) {
-//       recipes = await findConditionalNodes(Models.Recipe, 
-//         `t.name IN [${ingredients.split(',').map(i => `'${i.trim()}'`).join(',')}]`, 
-//         Relationships.HAS_INGREDIENT, 
-//         'direction_out', 
-//         Models.Ingredient
-//       )
-//     } else {
-//       recipes = await findAllNodes(Models.Recipe)
-//     }
-//     console.log('RECIPE RES SEND ', recipes)
-//     res.send(recipes);
-//   } 
-//   catch(error) {
-//     next(error);
-//   }
-// });
-
-// ************ Jarret's testing something
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => { 
+  let recipes;
   const { ingredients } = req.query;
-  const dbQuery = `
-  with [${ingredients.split(',').map(i => `'${i.trim()}'`).join(',')}] as names
-  match (i:Ingredient) <- [:HAS_INGREDIENT] - (r:Recipe)
-  where i.name in names
-  with r, size(names) as inputCnt, count(distinct i) as cnt
-  where cnt >= 1
-  return r
-  order by cnt desc limit 10
-  `;
-  neode.cypher(dbQuery)
-  .then(results => {
-    return neode.hydrate(results, 'r').toJson()
-  })
-  .then(json => {
-    console.log(json)
-    res.send(json)
-  })
+  try {
+    if(ingredients) {
+      recipes = await findConditionalNodes(
+        Models.Recipe, 
+        `t.name IN [${ingredients.split(',').map(i => `'${i.trim()}'`).join(',')}]`, 
+        Relationships.HAS_INGREDIENT, 
+        'direction_out', 
+        Models.Ingredient
+      )
+    } else {
+      recipes = await findAllNodes(Models.Recipe)
+    }
+    console.log('RECIPE RES SEND ', recipes)
+    res.send(recipes);
+  } 
+  catch(error) {
+    next(error);
+  }
 });
-// ************ End of Jarret's testing
 
 router.put('/:id/review/:userId', (req, res, next) => { 
   const { rating, description } = req.body;
