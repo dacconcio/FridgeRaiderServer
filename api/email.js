872072585aacd164edf2e4906393f6980f499607
Jarret Rose <jarretrose@gmail.com>
2018-12-03 18:22:58 -0500

    added nodemailer config and route for sending recipe to user via email

---
+++ api/email.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const { gmailAccount, gmailPassword } = require('../env')
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: gmailAccount,
+    pass: gmailPassword
+  }
+});
+
+router.post('/send', (req, res, next) => {
+
+  const mailOptions = {
+    from: gmailAccount,
+    to: `${req.body.targetEmail}`,
+    subject: `${req.body.emailSubject}`,
+    text: `${req.body.emailText}`
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  })
+})
+
+module.exports = router;
