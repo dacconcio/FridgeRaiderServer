dfa6dfcce8400cbd7d7413319d488313c69c3315
Jarret Rose <jarretrose@gmail.com>
2018-12-03 21:08:07 -0500

    process env fix

--- api/email.js
+++ api/email.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const { gmailAccount, gmailPassword } = require('../env')
+const { gmailAccount, gmailPassword } = require('../config'); 
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
