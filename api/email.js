651529bde08ce0df4c56b8c626ba0b59be5f5a51
Jarret Rose <jarretrose@gmail.com>
2018-12-04 12:43:01 -0500

    update share via email feature

--- api/email.js
+++ api/email.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const { gmailAccount, gmailPassword } = require('../config'); 
+const { gmailAccount, gmailPassword } = require('../config');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -13,21 +13,34 @@ const transporter = nodemailer.createTransport({
 });
 
 router.post('/send', (req, res, next) => {
-
+  console.log(`SENDING EMAIL at ${Date.now()}`)
   const mailOptions = {
     from: gmailAccount,
     to: `${req.body.targetEmail}`,
-    subject: `${req.body.emailSubject}`,
-    text: `${req.body.emailText}`
+    subject: 'I just raided my fridge!',
+    text: `
+      Check out this recipe that I found on the Fridge Raider App!\n
+      ${req.body.recipeURL}`,
+    html: `
+    <h3>Hey ${req.body.recipientName}!</h3>
+    <p>Check out this recipe for <a href=${req.body.recipeURL}>${req.body.emailRecipeName}</a> that I found on the Fridge Raider App!</p>
+      <a href=${req.body.recipeURL}><img src=${req.body.emailImg} alt="Picture of ${req.body.recipeName}" style="width:240px;height:240px;"></a>
+      <br>
+      <p>Happy Raiding!</p>
+      ${req.body.emailFromName}
+    `
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
+      res.sendStatus(500);
     } else {
       console.log('Email sent: ' + info.response);
+      transporter.close();
+      res.sendStatus(200);
     }
-  })
-})
+  });
+});
 
 module.exports = router;
